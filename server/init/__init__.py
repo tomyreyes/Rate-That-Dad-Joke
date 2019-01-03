170374f817188e20d7ca7f50b847d7ba9abf1b81
Tomy <tomyleereyes@gmail.com>
2019-01-03 14:35:18 -0800

    add route for dadjoke api

--- server/init/__init__.py
+++ server/init/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
 from init import routes
+
+from flask_alchemy import SQLAlchemy
--- server/init/routes.py
+++ server/init/routes.py
@@ -1,5 +1,25 @@
 from init import app
+from flask import jsonify
+import requests
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/get-joke', methods=['GET'])
+def getJoke():
+    url = 'https://icanhazdadjoke.com/'
+    headers = { 'Accept': 'application/json' }
+    try: 
+        joke = requests.get(url, headers=headers)
+        joke_object = joke.json()
+        joke.raise_for_status()
+        return jsonify({'id':joke_object['id'],  'joke': joke_object['joke']})
+    except requests.exceptions.HTTPError as err:
+        return jsonify({'message': err})
+
+#  @app.route('/get-rating', methods=['GET'])
+#  def getRating():
+
+
+
