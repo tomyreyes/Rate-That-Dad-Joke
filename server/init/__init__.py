942bc9515e7af8ea0fa864028869172e977964d9
Tomy <tomyleereyes@gmail.com>
2019-01-03 17:35:55 -0800

    add Joke model

--- server/init/__init__.py
+++ server/init/__init__.py
@@ -1,5 +1,17 @@
+#!/usr/bin/python
 from flask import Flask
 app = Flask(__name__)
 from init import routes
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sites.db'
 
-from flask_alchemy import SQLAlchemy
+db = SQLAlchemy(app)
+
+class Joke(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    joke = db.Column(db.String(250), unique=True, nullable=False)
+    ratings = db.Column(db.PickleType)
+
+    def __repr__(self):
+        return f"Joke('{self.joke}', '{self.ratings}')"
+        
--- server/run.py
+++ server/run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from init import app
 if __name__ == '__main__':
     app.run(debug=True)
